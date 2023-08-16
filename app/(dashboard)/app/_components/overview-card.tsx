514857429441f42ffeed6fddde2c496e9d0b969b
Francis Masha <francismasha96@gmail.com>
2023-08-16 15:05:33 +0300

    feat: improve font views

--- app/(dashboard)/app/_components/overview-card.tsx
+++ app/(dashboard)/app/_components/overview-card.tsx
@@ -19,7 +19,9 @@ export default function OverviewCard({
 	return (
 		<Card>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-				<CardTitle className='text-sm font-normal'>{heading}</CardTitle>
+				<CardTitle className='text-sm font-semibold text-muted-foreground'>
+					{heading}
+				</CardTitle>
 				<Icon className='h-5 w-5' />
 			</CardHeader>
 			<CardContent>
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,12 +1,44 @@
 import { JetBrains_Mono } from 'next/font/google';
-import localFont from 'next/font/local'; // export const fontSans = localFont({
+import localFont from 'next/font/local'; //
 
 export const fontSans = localFont({
-	src: '../assets/fonts/sf-pro-text-regular.woff2',
-	weight: '500',
+	src: [
+		{
+			path: '../assets/fonts/sf-pro-text-regular-webfont.woff2',
+			weight: '400',
+			style: 'light',
+		},
+		{
+			path: '../assets/fonts/sf-pro-text-regular-webfont.woff2',
+			weight: '500',
+			style: 'normal',
+		},
+		{
+			path: '../assets/fonts/sf-pro-text-semibold-webfont.woff2',
+			weight: '600',
+			style: 'semibold',
+		},
+		{
+			path: '../assets/fonts/sf-pro-text-medium-webfont.woff2',
+			weight: '600',
+			style: 'medium',
+		},
+		{
+			path: '../assets/fonts/sf-pro-text-bold-webfont.woff2',
+			weight: '700',
+			style: 'bold',
+		},
+	],
 	variable: '--font-sans',
 	display: 'swap',
 });
+
+// export const fontSans = localFont({
+// 	src: '../assets/fonts/sf-pro-text-regular-webfont.woff2',
+// 	weight: '500',
+// 	variable: '--font-sans',
+// 	display: 'swap',
+// });
 
 // Font files can be collocated inside `pages`
 export const fontHeading = localFont({
