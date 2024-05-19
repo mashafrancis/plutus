458ff3a823e466c03a7430e59a2484528e4d8fa9
Francis Masha <francismasha96@gmail.com>
2024-05-19 08:35:21 +0300

    feat: fix drawer vaul

--- apps/web/components/layout/app-nav.tsx
+++ apps/web/components/layout/app-nav.tsx
@@ -1,36 +1,51 @@
 import Link from 'next/link'
 
+import { auth } from '@/auth'
 import SectionContainer from '@/components/layout/section-container'
+import { UserMenu } from '@/components/user/user-menu'
 import navigation from '@/data/navigation'
+import { Session } from '@/lib/types'
 import { cn } from '@/lib/utils'
 import { NAVIGATION_NAMES } from '@/types/navigation'
+import { Suspense } from 'react'
 
 interface Props {
   activePage: NAVIGATION_NAMES
 }
 
+async function User() {
+  const session = (await auth()) as Session
+
+  return session ? <UserMenu user={session.user} /> : null
+}
+
 export default function AppNav({ activePage }: Props) {
   return (
     <nav className="relative z-20 hidden md:flex items-center w-full border-b">
-      <SectionContainer className="!py-0 flex gap-3 items-center">
-        {Object.entries(navigation).map((obj: any) => {
-          const site = obj[1]
+      <SectionContainer className="flex !py-0 justify-between">
+        <div className="flex gap-3 items-center">
+          {Object.entries(navigation).map((obj: any) => {
+            const site = obj[1]
 
-          return (
-            <Link
-              key={site.name}
-              className={cn(
-                'flex items-center gap-1.5 px-2 first:-ml-2 py-4 border-b border-transparent text-sm text-muted-foreground hover:text-foreground',
-                'focus-visible:ring-2 focus-visible:ring-muted-foreground focus-visible:text-foreground focus-visible:outline-primary/60',
-                site.name === activePage &&
-                  'border-muted-foreground text-foreground',
-              )}
-              href={`/${site.name.toLowerCase().replace(' ', '-')}`}
-            >
-              <p>{site.name}</p>
-            </Link>
-          )
-        })}
+            return (
+              <Link
+                key={site.name}
+                className={cn(
+                  'flex items-center gap-1.5 px-2 first:-ml-2 py-4 border-b border-transparent text-sm text-muted-foreground hover:text-foreground',
+                  'focus-visible:ring-2 focus-visible:ring-muted-foreground focus-visible:text-foreground focus-visible:outline-primary/60',
+                  site.name === activePage &&
+                    'border-muted-foreground text-foreground',
+                )}
+                href={`/${site.name.toLowerCase().replace(' ', '-')}`}
+              >
+                <p>{site.name}</p>
+              </Link>
+            )
+          })}
+        </div>
+        <Suspense fallback={<div className="flex-1 overflow-auto" />}>
+          <User />
+        </Suspense>
       </SectionContainer>
     </nav>
   )
---
+++ apps/web/lib/types.ts
@@ -0,0 +1,6 @@
+export interface Session {
+  user: {
+    id: string
+    email: string
+  }
+}
