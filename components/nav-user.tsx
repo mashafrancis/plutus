84ef1cd543cf6fc58ce6e4183e867bbb5e2d4d39
Francis Masha <francismasha96@gmail.com>
2025-06-17 06:44:16 +0300

    feat: add sentry

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -6,10 +6,11 @@ import {
   ChevronsUpDown,
   CreditCard,
   LogOut,
-  Sparkles,
 } from "lucide-react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {authClient} from "@/auth/client";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Button} from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -25,86 +26,101 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import {useRouter} from "next/navigation";
 
 export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
+	                        user,
+                        }: {
+	user: {
+		name: string;
+		email: string;
+		avatar: string;
+	};
 }) {
-  const { isMobile } = useSidebar();
+	const {isMobile} = useSidebar();
+	const router = useRouter();
 
-  return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
-                </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <BadgeCheck />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <LogOut />
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
-    </SidebarMenu>
-  );
+	return (
+		<SidebarMenu>
+			<SidebarMenuItem>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<SidebarMenuButton
+							size="lg"
+							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+						>
+							<Avatar className="h-8 w-8 rounded-lg">
+								<AvatarImage src={user.avatar} alt={user.name}/>
+								<AvatarFallback className="rounded-lg">PF</AvatarFallback>
+							</Avatar>
+							<div className="grid flex-1 text-left text-sm leading-tight">
+								<span className="truncate font-medium">{user.name}</span>
+								<span className="truncate text-muted-foreground text-xs">
+                  {user.email}
+                </span>
+							</div>
+							<ChevronsUpDown className="ml-auto size-4"/>
+						</SidebarMenuButton>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent
+						className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+						side={isMobile ? "bottom" : "right"}
+						align="end"
+						sideOffset={4}
+					>
+						<DropdownMenuLabel className="p-0 font-normal">
+							<div
+								className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+								<Avatar className="h-8 w-8 rounded-lg">
+									<AvatarImage src={user.avatar} alt={user.name}/>
+									<AvatarFallback className="rounded-lg">PF</AvatarFallback>
+								</Avatar>
+								<div className="grid flex-1 text-left text-sm leading-tight">
+									<span className="truncate font-medium">{user.name}</span>
+									<span className="truncate text-muted-foreground text-xs">
+                    {user.email}
+                  </span>
+								</div>
+							</div>
+						</DropdownMenuLabel>
+						<DropdownMenuSeparator/>
+						<DropdownMenuGroup>
+							<DropdownMenuItem>
+								<BadgeCheck/>
+								Account
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<CreditCard/>
+								Billing
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<Bell/>
+								Notifications
+							</DropdownMenuItem>
+						</DropdownMenuGroup>
+						<DropdownMenuSeparator/>
+
+						<DropdownMenuItem>
+							<Button
+								variant="destructive"
+								className="w-full"
+								onClick={() => {
+									authClient.signOut({
+										fetchOptions: {
+											onSuccess: () => {
+												router.push("/"); // redirect to home page
+											},
+										},
+									});
+								}}
+							>
+								<LogOut className="text-destructive-foreground"/>
+								Log out
+							</Button>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</SidebarMenuItem>
+		</SidebarMenu>
+	);
 }
--- next.config.ts
+++ next.config.ts
@@ -3,45 +3,45 @@
  * for Docker builds.
  */
 import "./env";
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const config: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.google.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "plutus.francismasha.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  compiler: {
-    removeConsole: true,
-  },
-  logging: {
-    fetches: {
-      fullUrl: true,
-      hmrRefreshes: true,
-    },
-  },
-  poweredByHeader: false,
-  typescript: { ignoreBuildErrors: true },
-  async headers() {
-    return [{ source: "/(.*)", headers: securityHeaders }];
-  },
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "www.google.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "plutus.francismasha.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+	compiler: {
+		removeConsole: true,
+	},
+	logging: {
+		fetches: {
+			fullUrl: true,
+			hmrRefreshes: true,
+		},
+	},
+	poweredByHeader: false,
+	typescript: {ignoreBuildErrors: true},
+	async headers() {
+		return [{source: "/(.*)", headers: securityHeaders}];
+	},
 };
 
 const ContentSecurityPolicy = `
     default-src 'self' francismasha.com;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.francismasha.com *.vercel-scripts.com fonts.googleapis.com fonts.gstatic.com *.cloudfront.net api.seline.com openpanel.dev cdn.seline.so assets.lemonsqueezy.com *.googletagmanager.com accounts.google.com https://vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.francismasha.com *.vercel-scripts.com fonts.googleapis.com fonts.gstatic.com *.cloudfront.net api.seline.com openpanel.dev cdn.seline.so assets.lemonsqueezy.com *.googletagmanager.com accounts.google.com vercel.live;
     child-src 'self' plutus.lemonsqueezy.com;
     style-src 'self' 'unsafe-inline' fonts.googleapis.com;
     img-src * blob: data:;
@@ -52,41 +52,41 @@ const ContentSecurityPolicy = `
 `;
 
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: "X-Frame-Options",
-    value: "DENY",
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=31536000; includeSubDomains; preload",
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), autoplay=()",
-  },
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+	{
+		key: "Referrer-Policy",
+		value: "origin-when-cross-origin",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+	{
+		key: "X-Frame-Options",
+		value: "DENY",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=31536000; includeSubDomains; preload",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+	{
+		key: "Permissions-Policy",
+		value: "camera=(), microphone=(), geolocation=(), autoplay=()",
+	},
 ];
 
 export default config;
