1cec2f686bc5b4ea626bb43cd64485bc6d19b4d0
Francis Masha <francismasha96@gmail.com>
2025-11-20 23:30:01 +0300

    chore: upgrade prisma to v7

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,16 @@
-import path from 'node:path';
-import {defineConfig} from "prisma/config";
-
-import 'dotenv/config';
-
-type Env = {
-	DATABASE_URL: string;
-};
+import "dotenv/config";
+import path from "node:path";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-	schema: path.join('prisma', 'schema'),
-	experimental: {
-		studio: true,
-		adapter: true
-	}
-})
+  schema: path.join('prisma', 'schema'),
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url:
+      (process.env.DIRECT_URL as string) ||
+      (process.env.DATABASE_URL as string),
+    directUrl: process.env.DIRECT_URL as string,
+  },
+});
