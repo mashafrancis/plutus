8cae80bd56d5bd91855324b3bcc31dc76de19b56
Francis Masha <francismasha96@gmail.com>
2025-11-26 21:23:58 +0300

    chore: update packages

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -2,11 +2,12 @@ import "dotenv/config";
 import { PrismaClient } from "@/generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL as string;
 
 export function getDb() {
   // const adapter = new PrismaPg({connectionString});
   return new PrismaClient({
+    accelerateUrl: connectionString,
       log: process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
