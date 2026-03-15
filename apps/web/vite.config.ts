1fc96f7a53f7cfcb07af47d31f93b246aa8c6089
Francis Masha <francismasha96@gmail.com>
2026-03-15 23:15:27 +0300

    chore: deploy to vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,16 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
+import { sentryTanstackStart } from "@sentry/tanstackstart-react/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import {sentryTanstackStart} from "@sentry/tanstackstart-react/vite";
-import { nitro } from 'nitro/vite'
+
+const nitroPreset = process.env.VERCEL ? "vercel" : "node-server";
 
 export default defineConfig({
-  plugins: [tailwindcss(), nitro({ preset: 'node-server' }), tanstackStart(), viteReact(), sentryTanstackStart({
-    org: "plutus-finance",
-    project: "plutus-finance",
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-  }),],
+  plugins: [
+    tailwindcss(),
+    nitro({ preset: nitroPreset }),
+    tanstackStart(),
+    viteReact(),
+    sentryTanstackStart({
+      org: "plutus-finance",
+      project: "plutus-finance",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
   server: {
     port: 3000,
     strictPort: true,
