73ba3c4423703537e355d73ec3ce2e756adcf0db
Francis Masha <francismasha96@gmail.com>
2026-03-16 23:46:13 +0300

    chore: updates vite plus

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,26 @@ import { defineConfig } from "vite-plus";
 
 export default defineConfig({
   lint: { options: { typeAware: true, typeCheck: true } },
+  fmt: {
+    ignorePatterns: [
+      "**/dist",
+      "**/.turbo",
+      "**/dev-dist",
+      "**/routeTree.gen.ts",
+      "**/convex/_generated",
+      "**/.next",
+      "**/.zed",
+      "**/.vscode",
+      "**/src-tauri",
+      "**/.nuxt",
+      "**/.expo",
+      "**/.wrangler",
+      "**/.alchemy",
+      "**/.svelte-kit",
+      "**/.source",
+    ],
+  },
   staged: {
-    "*.{js,jsx,ts,tsx,json,jsonc,css,scss,md,mdx}": ["pnpm dlx ultracite fix"],
+    "*.{js,jsx,ts,tsx,json,jsonc,css,scss,md,mdx}": ["vp check --fix"],
   },
 });
